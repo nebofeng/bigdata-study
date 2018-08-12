b9c3716eea6b5b8b43ac0111a73a2172e5b0ebb0
nebofeng <nebofeng@gmail.com>
2018-08-12 19:08:31 +0800

    --KafkaStreams WordCount 示例代码

---
+++ kafka1.0.0_study/src/main/java/com/nebo/kafkastreams/KafkaCountNumberProducer.java
@@ -0,0 +1,20 @@
+package com.nebo.kafkastreams;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaCountNumberProducer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaCountNumberProducer.class);
+
+    public static void main(String[] args) {
+     //初始化配置信息
+
+     //生产者比较简单，这里使用 控制台作为生产者。
+
+
+
+
+
+
+    }
+}
