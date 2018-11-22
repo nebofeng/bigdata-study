50e97ee72683c1b3937183cd812181c965a42f2e
ningbo <nebofeng@gmail.com>
2018-11-22 23:01:49 +0800

    --Spark hdfs文件输入输出

---
+++ bigdata-spark/src/main/java/pers/nebo/spark/OutPutFiles.java
@@ -0,0 +1,36 @@
+package pers.nebo.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+/**
+ * @auther nebofeng
+ * @email nebofeng@gmail.com
+ * @date 2018/11/22
+ * @des : spark 输出文件
+
+ */
+public class OutPutFiles {
+    public static void main(String[] args) {
+        SparkConf sc = new SparkConf();
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sc);
+        JavaRDD<String> javaRDD = javaSparkContext.textFile("");
+        javaRDD.mapToPair(new PairFunction<String, Object, Object>() {
+            @Override
+            public Tuple2<Object, Object> call(String s) {
+                return null;
+            }
+        }).reduceByKey(new Function2<Object, Object, Object>() {
+            @Override
+            public Object call(Object v1, Object v2) {
+                return null;
+            }
+        }).saveAsHadoopFile("",);
+
+    }
+}
