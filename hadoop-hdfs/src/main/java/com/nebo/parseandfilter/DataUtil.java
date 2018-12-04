e997fb2b285d9dde84db1680da0b0364454b5da3
ningbo <nebofeng@gmail.com>
2018-12-04 10:27:32 +0800

    --flume数据采集，mr清洗转换

---
+++ hadoop-hdfs/src/main/java/com/nebo/parseandfilter/DataUtil.java
@@ -0,0 +1,102 @@
+package com.nebo.parseandfilter;
+import java.net.URLDecoder;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/**
+ * 
+ * 解析机顶盒用户原始数据
+ * <GHApp>
+ * <WIC cardNum="174041665" stbNum="01050908200014994" 
+ * date="2012-09-16" pageWidgetVersion="1.0">
+ * <A e="23:56:45" s="23:51:45" n="133" t="2" pi="488" 
+ * p="24%E5%B0%8F%E6%97%B6" sn="CCTV-13 新闻" />
+ * </WIC>
+ * </GHApp>
+ *
+ */
+public class DataUtil {
+
+	@SuppressWarnings("unchecked")
+	public static void transData(String text,Context context) {
+		try {
+			//通过Jsoup解析每行数据
+			Document doc = Jsoup.parse(text);
+			
+			//获取WIC标签内容，每行数据只有一个WIC标签
+			Elements content = doc.getElementsByTag("WIC");
+
+			//解析出机顶盒号
+			String stbNum = content.get(0).attr("stbNum");
+			if(stbNum == null||"".equals(stbNum)){
+				return ;
+			}
+			
+			//解析出日期
+			String date = content.get(0).attr("date");
+			
+			if(date == null||"".equals(date)){
+				return ;
+			}
+
+			//解析A标签
+			Elements els = doc.getElementsByTag("A");
+
+			for (Element el : els) {
+				//解析结束时间
+				String e = el.attr("e");
+				if(e ==null||"".equals(e)){
+					break;
+				}
+				//解析起始时间
+				String s = el.attr("s");
+				if(s == null||"".equals(s)){
+					break;
+				}
+
+				//解析节目内容
+				String p = el.attr("p");
+				if(p == null||"".equals(p)){
+					break;
+				}
+
+				//解析频道
+				String sn = el.attr("sn");
+				
+				if(sn ==null||"".equals(sn)){
+					break ;
+				}
+
+				//对节目解码
+				p = URLDecoder.decode(p, "utf-8");
+
+				//解析出统一的节目名称，比如：天龙八部(1)，天龙八部(2)，同属于一个节目
+				int index = p.indexOf("(");
+
+				if (index != -1) {
+					p = p.substring(0, index);
+				} 
+
+				//起始时间转换为秒
+				int startS = TimeUtil.TimeToSecond(s);
+				
+				//结束时间转换为秒
+				int startE = TimeUtil.TimeToSecond(e);
+
+				if (startE < startS) {
+					startE = startE + 24 * 3600;
+				}
+				//每条记录的收看时长
+				int duration = startE - startS;
+
+				context.write(new Text(stbNum + "@" + date), new Text(sn + "@" + p+ "@" + s + "@" + e + "@" + duration));
+
+			}
+		} catch (Exception e) {
+               e.printStackTrace();
+		}
+	}
+}
---
+++ hadoop-hdfs/src/main/java/com/nebo/parseandfilter/ExtractCurrentNum.java
@@ -0,0 +1,114 @@
+package com.nebo.parseandfilter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+/**
+ * 
+ * 针对上一步的结果统计每分钟的当前在播数
+ * 
+ */
+public class ExtractCurrentNum  extends Configured implements Tool {
+	public static class ExtractCurrentNumMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		@Override
+		protected void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			// stbNum0+"@"+date1+"@"+sn2+"@"+p3+"@"+s4+"@"+e5+"@"+duration6
+			String[] kv = StringUtils.split(value.toString(), "@");
+			// 过滤掉不合格数据
+			if (kv.length != 7) {
+				return;
+			}
+			// 机顶盒号
+			String stbnum = kv[0].trim();
+			// 日期
+			String date = kv[1].trim();
+
+			// 将时间段解析为每分钟记录，比如23:51:45~23:56:45之间的每分钟
+			List<String> list = TimeUtil.getTimeSplit(kv[4], kv[5]);
+			int size = list.size();
+			// 循环统计所有指标每分钟的数据
+			for (int i = 0; i < size; i++) {
+				// 根据start end 切割的每分钟
+				String min = list.get(i);
+
+				// 输出每分钟当前在播人数（1）
+				context.write(new Text(date + "@" + min), new Text(stbnum));
+			}
+		}
+	}
+
+	public static class ExtractCurrentNumReduce extends
+			Reducer<Text, Text, Text, Text> {
+		private Text result = new Text();
+		// 定义当前在播数集合
+		private Set<String> set_curnum = new HashSet<String>();
+
+		protected void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			set_curnum.clear();
+			for (Text value : values) {
+				set_curnum.add(value.toString());
+			}
+			// 计算出当前在播人数
+			result.set(set_curnum.size()+"");
+			context.write(key, result);
+		}
+
+	}
+
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length < 2) {
+			System.err
+					.println("Usage: ExtractProgramCurrentNum [<in>...] <out>");
+			System.exit(2);
+		}
+
+		Job job = Job.getInstance();
+
+		// 设置输出key value分隔符
+		job.getConfiguration().set(
+				"mapreduce.output.textoutputformat.separator", "@");
+
+		job.setJarByClass(ExtractCurrentNum.class);
+		job.setMapperClass(ExtractCurrentNumMapper.class);
+		job.setReducerClass(ExtractCurrentNumReduce.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		// 设置输入路径
+		for (int i = 0; i < otherArgs.length - 1; ++i) {
+			FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+		}
+
+		// 设置输出路径
+		FileOutputFormat.setOutputPath(job, new Path(
+				otherArgs[otherArgs.length - 1]));
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int ec = ToolRunner.run(new Configuration(),
+				new ExtractCurrentNum(), args);
+		System.exit(ec);
+	}
+}
