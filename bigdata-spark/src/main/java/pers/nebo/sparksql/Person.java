eafeb7fbc5917ea90ae8f4592cefa8599614a214
nebofeng <nebofeng@gmail.com>
2019-08-19 23:29:01 +0800

    [ADD] 实体类java版本

---
+++ bigdata-spark/src/main/java/pers/nebo/sparksql/Person.java
@@ -0,0 +1,33 @@
+package pers.nebo.sparksql;
+
+/**
+ * @ author fnb
+ * @ email nebofeng@gmail.com
+ * @ date  2019/8/19
+ * @ des :
+ */
+public class Person {
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int age;
+    public String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
