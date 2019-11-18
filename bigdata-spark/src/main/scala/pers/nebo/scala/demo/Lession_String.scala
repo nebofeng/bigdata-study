c4baef2e4e4d781f741b53c9dc41a67af2015f86
nebofeng <nebofeng@gmail.com>
2019-11-19 00:51:23 +0800

    [ADD] scala string

---
+++ bigdata-spark/src/main/scala/pers/nebo/scala/demo/Lession_String.scala
@@ -0,0 +1,17 @@
+package pers.nebo.scala.demo
+
+/**
+  * @ author fnb
+  * @ email nebofeng@gmail.com
+  * @ date  2019/11/19
+  * @ des :
+  */
+object Lession_String {
+  def main(args: Array[String]): Unit = {
+     val s="demo"
+     val s1="DEMO"
+     println(s.indexOf(98))
+     println(s.equals(s1))
+     println(s.equalsIgnoreCase(s1))
+  }
+}
