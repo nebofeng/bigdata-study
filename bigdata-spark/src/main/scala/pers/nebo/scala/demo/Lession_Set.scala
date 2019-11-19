f68469c79e8eb72ae531e59efe839f6febc2fb5e
nebofeng <nebofeng@gmail.com>
2019-11-19 23:12:51 +0800

    [ADD] Scala Set

---
+++ bigdata-spark/src/main/scala/pers/nebo/scala/demo/Lession_Set.scala
@@ -0,0 +1,52 @@
+package pers.nebo.scala.demo
+
+/**
+  * @ author fnb
+  * @ email nebofeng@gmail.com
+  * @ date  2019/11/19
+  * @ des :
+  */
+
+import scala.collection.mutable;
+import scala.collection.immutable;
+object Lession_Set {
+
+  def main(args: Array[String]): Unit = {
+    //    val set = mutable.Set[Int](1,2,3)
+    //    set.+=(100)
+    //    set.foreach(println)
+    //    val set1 = immutable.Set[String]("a","b")
+
+    //    val set = Set[Int](1,2,3,4)
+    //    val set1 = Set[Int](3,4,5,6)
+
+    //    val ints: Set[Int] = set.filter(elem => {
+    //      elem >= 2
+    //    })
+    //    ints.foreach(println)
+
+
+
+
+//    val set = Set[Int](1, 2, 3, 4)
+//    val set1 = Set[Int](3, 4, 5, 6)
+
+    //    //set1中有，set中没有的集合，与set1 &~set1
+    //    val setd2 =set1 &~set
+    //    setd2.foreach(println)
+
+    //    val setd=set1.diff(set)
+    //    setd.foreach(line=>{println(line)})
+    //     val ints:Set[Int] =set.intersect(set1)
+
+    //两个set的交集
+    //    //两个set的交集 与set & set1 作用一样
+    //    ints.foreach(elem=>{println(elem)})
+
+
+    //    val set=Set[Int](1,2,3,4,5)
+    //    for(elem<-set){
+    //      println(elem)
+    //    }
+  }
+}
