408fc20ae89105b190f926d5faf0c5b6304b795f
nebofeng <nebofeng@gmail.com>
2019-11-19 22:25:42 +0800

    [ADD] Scala List

---
+++ bigdata-spark/src/main/scala/pers/nebo/scala/demo/Lession_List.scala
@@ -0,0 +1,62 @@
+package pers.nebo.scala.demo
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * @ author fnb
+  * @ email nebofeng@gmail.com
+  * @ date  2019/11/19
+  * @ des :
+  */
+object Lession_List {
+  def main(args: Array[String]): Unit = {
+
+//     可变list
+//     val list = ListBuffer[Int](1,2,3)
+//    list.append(4,5,6)
+//    list.+=:(100)
+//    list.foreach(println)
+
+
+
+    // length < 4 的count
+
+//      val list = List[String]("hello scala","hello java","hello spark","a","abc")
+//
+//        val i: Int = list.count(s => {
+//          s.length < 4
+//        })
+//        println(i)
+
+
+    //    true的留下
+    //    val result: List[String] = list.filter(s => {
+    //      "hello scala".equals(s)
+    //    })
+    //    result.foreach(println)
+
+
+
+    //    val result: List[Array[String]] = list.map(s => {
+    //      s.split(" ")
+    //    })
+
+    //    val result: List[String] = list.flatMap(s=>{s.split(" ")})
+    //    result.foreach(println)
+
+
+    //    result.foreach(arr=>{
+    //      println("新的数组")
+    //      arr.foreach(println)
+    //    })
+
+
+
+    //    val list = List[Int](1,2,3)
+    //    for(elem<-list){
+    //      println(elem)
+    //    }
+    //    list.foreach(println)
+  }
+
+}
