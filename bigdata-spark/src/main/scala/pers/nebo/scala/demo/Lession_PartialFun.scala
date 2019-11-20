2f159316f13f57a66855f57342ac9fc06f650527
nebofeng <nebofeng@gmail.com>
2019-11-20 22:26:45 +0800

    [ADD] Scala PartialFun

---
+++ bigdata-spark/src/main/scala/pers/nebo/scala/demo/Lession_PartialFun.scala
@@ -0,0 +1,24 @@
+package pers.nebo.scala.demo
+
+/**
+  * @ author fnb
+  * @ email nebofeng@gmail.com
+  * @ date  2019/11/20
+  * @ des :
+  */
+
+/**
+  * 偏函数，只能匹配一个值，匹配上了放回某个值
+  * PartialFunction[A,B]  A是匹配的类型，B是匹配上返回的类型
+  */
+object Lession_PartialFun {
+  def MyTest :PartialFunction[String,Int] ={
+    case "abc"=>2
+    case "a" => 1
+    case _ => 200
+  }
+  def main(args: Array[String]): Unit = {
+    val result: Int = MyTest("abcd")
+    println(result)
+  }
+}
