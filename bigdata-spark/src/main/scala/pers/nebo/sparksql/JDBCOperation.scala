e0fbbf8ab3007c96e7ec6b44878407f339123e49
nebofeng <nebofeng@gmail.com>
2019-12-05 23:52:39 +0800

    [UPDATE] spark sql jdbc

--- bigdata-spark/src/main/scala/pers/nebo/sparksql/JDBCOperation.scala
+++ bigdata-spark/src/main/scala/pers/nebo/sparksql/JDBCOperation.scala
@@ -3,7 +3,7 @@ package pers.nebo.sparksql
 import java.util.Properties
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.{DataFrame, DataFrameReader, SQLContext, SaveMode, SparkSession}
 
 /**
   * @ author fnb
@@ -40,6 +40,49 @@ object JDBCOperation {
     val url = "jdbc:mysql://hadoop1:3306/xtwy"
     val stduentDF=spark.read.jdbc(url, "student", properties)
     stduentDF.show();
+
+
+   //第二种方式
+
+    val map =Map[String,String](
+      "url"->"",
+       "driver"->"",
+       "user"->"",
+       "passwd"->"",
+    "dbtable"->""//表名
+
+    )
+
+    val score:DataFrame = spark.read.format("jdbc").options(map).load()
+
+    // 第三种方式
+
+    val reader: DataFrameReader =spark.read.format("jdbc")
+      .option("url","")
+      .option("dirver","")
+      .option("user","")
+      .option("password","123456")
+      .option("dbtable","score")
+    val score2:DataFrame =reader.load()
+    score2.show()
+
+     //将上马两张表注册成为临时表 做关联查询
+
+    stduentDF.createOrReplaceTempView("student")
+    score.createOrReplaceTempView("score")
+    spark.sql("").show()
+
+
+   //将结果保存再 mysql 中 ,String 格式默认为 text ，如果不使用这个格式 ，可以自己建表创建各个列的格式再保存
+
+    val result:DataFrame=spark.sql("")
+    result.write.mode(SaveMode.Append).jdbc("","",properties)
+
+
+    //第四种方法
+
+    spark.read.jdbc("jdbc:","(sql )t ",properties)
+
 
   }
 
