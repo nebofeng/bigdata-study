bd6d2649607d7c5d2fa235158b40e137cb55177c
nebofeng <nebofeng@gmail.com>
2020-01-02 23:00:30 +0800

    [UPDATE] 增加了一种方法

--- bigdata-kafka/src/main/java/pers/nebo/kafkaoffset/KafkaGetLastOffset.java
+++ bigdata-kafka/src/main/java/pers/nebo/kafkaoffset/KafkaGetLastOffset.java
@@ -19,42 +19,70 @@ public class KafkaGetLastOffset {
 
     public static void main(String[] args) {
 
-
+        String topic="test-topic";
 
         Properties props = new Properties();
         props.put("bootstrap.servers", "node1:6667");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(props);
+        List<TopicPartition> tps =new ArrayList<TopicPartition>();
+
+        List<PartitionInfo> partitionInfoList=kafkaConsumer.partitionsFor("test");
+        for(PartitionInfo partitionInfo:partitionInfoList){
+            tps.add(new TopicPartition(topic,partitionInfo.partition()));
+        }
+
+
         /*
          method1  参考博客做了一些修改，修改了认为不对的地方。
          https://www.cnblogs.com/cocowool/p/get_kafka_latest_offset.html
         */
-        KafkaConsumer kafkaConsumer = new KafkaConsumer(props);
-        List<TopicPartition> tp =new ArrayList<TopicPartition>();
-
-        List<PartitionInfo> partitionInfoList=kafkaConsumer.partitionsFor("test");
-        for(PartitionInfo partitionInfo:partitionInfoList){
-            tp.add(new TopicPartition("test",partitionInfo.partition()));
-        }
-        kafkaConsumer.assign(Collections.singletonList(tp));
-        kafkaConsumer.seekToEnd(Collections.singletonList(tp));
-
-        for(TopicPartition topicPartition:tp){
-
-            System.out.println("Partition " + topicPartition.partition()
-            + " 's latest offset is '" + kafkaConsumer.position(topicPartition));
-
-        }
-
+       method1(topic,props,tps);
 
       /*
         method 2
         https://blog.csdn.net/chenggongdeli11/article/details/93502206
       */
 
+       method2(topic,props,tps);
 
+     }
 
+    /**
+     * by seek to end method
+     * @param topic
+     * @param props
+     * @param tps
+     */
+     public static  void method1(String topic,Properties props,List <TopicPartition> tps){
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(props);
+        kafkaConsumer.assign(Arrays.asList(topic));
+        kafkaConsumer.seekToEnd(tps);
+        for(TopicPartition topicPartition:tps){
+            System.out.println("Partition " + topicPartition.partition()
+                    + " 's latest offset is '" + kafkaConsumer.position(topicPartition));
+
+        }
+
+    }
+
+    /**
+     * by endoffsets method
+     * @param topic
+     * @param props
+     * @param tps
+     */
+
+    public static void method2(String topic,Properties props,List <TopicPartition> tps) {
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(props);
+
+        Map<TopicPartition,Long> topicPartitionOffsets=kafkaConsumer.endOffsets(tps);
+        for(Map.Entry entry:topicPartitionOffsets.entrySet()){
+            System.out.println(entry.getKey()+"=="+entry.getValue());
+
+        }
 
     }
 }
